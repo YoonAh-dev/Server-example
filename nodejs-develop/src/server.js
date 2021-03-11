78b80bc9efe2c70faaf862a752f35424bb4b9c64
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-03-12 00:17:54 +0900

    Change env

--- nodejs-develop/src/server.js
+++ nodejs-develop/src/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const dev = require('./dev');
 const { userRouter, blogRouter } = require('./routes');
 const { generateFakeData } = require('../faker2');
 
+
+
 const server = async() => {
     try {
-        await mongoose.connect(dev.mongoURI, 
+        const { MONGO_URI } = process.env;
+        if(!MONGO_URI) throw new Error("MONGO_URI is required!!")
+
+        await mongoose.connect(MONGO_URI, 
             { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }, (err) => {
             if(err) console.log({ err })
         });
